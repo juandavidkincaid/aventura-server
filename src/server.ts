fe35e0e7d34ce51062bfd8a1c0c67b04b30d7506
Juan David Kincaid <juandavidkincaid@gmail.com>
2021-06-26 14:30:54 -0500

    added mocks

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
+import fs from 'fs-extra';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import { ObjectId } from 'mongodb';
 
 import { Aventura, AventuraConfigThunk } from '@aventura-src/core';
+import * as gmodels from '@aventura-modules/models';
+
+// Running
 
 const mongod = new MongoMemoryServer();
 
@@ -13,14 +18,50 @@ const config: AventuraConfigThunk = async ()=>({
 
 const aventura = new Aventura(config);
 
+// Collect Static and Run Server
 (async ()=>{
     /* (async ()=>{
         await aventura.collectStatic('../staticFiles/', false);
-        console.log('Collected');
+        console.log('Static Collected');
     })(); */
 
     await aventura.runServer();
 })();
 
-export { Aventura, AventuraConfig } from '@aventura-src/core';
-export * as models from '@aventura-modules/models';
+// Add Restaurant Samples
+(async ()=>{
+    const gridfs = await aventura.gridfs;
+    const imageMediaIds: ObjectId[] = [];
+
+    for(let k = 1; k<=8; k++){
+        const mediaId = new ObjectId();
+        const imagePath = require(`@aventura-res/samples/sample${k}.png`);
+        const rStream = fs.createReadStream(`${__dirname}${imagePath}`);
+        const uploadStream = await gridfs.openUploadStreamWithId(mediaId, mediaId.toHexString(), {contentType: 'image/png'});
+        rStream.pipe(uploadStream);
+        imageMediaIds.push(mediaId);
+    }
+
+    for(let i = 1; i <= 15; i++){
+        
+        const restaurant = new gmodels.market.Restaurant({
+            name: `Sample (${i})`,
+            instagramProfile: `@sample${i}`,
+            phone: '333-444-1234',
+            image: {
+                mediaId: imageMediaIds[Math.floor(Math.random() * 1000) % imageMediaIds.length],
+                mime: 'image/png'
+            }
+        });
+
+        await restaurant.save();
+    }
+})();
+
+// Running
+
+export {
+    Aventura,
+    AventuraConfigThunk,
+    gmodels as models
+}
