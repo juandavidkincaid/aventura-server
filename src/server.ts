9372cf684967b06651c7c6131a974cc7d94d268d
Juan David Kincaid <juandavidkincaid@gmail.com>
2021-06-26 10:27:11 -0500

    added initialization

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,26 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+import { Aventura, AventuraConfigThunk } from '@aventura-src/core';
+
+const mongod = new MongoMemoryServer();
+
+const config: AventuraConfigThunk = async ()=>({
+    dev: true,
+    security: {
+        mongoDbUrl: await mongod.getUri(),
+    }
+});
+
+const aventura = new Aventura(config);
+
+(async ()=>{
+    /* (async ()=>{
+        await aventura.collectStatic('../staticFiles/', false);
+        console.log('Collected');
+    })(); */
+
+    await aventura.runServer();
+})();
+
 export { Aventura, AventuraConfig } from '@aventura-src/core';
 export * as models from '@aventura-modules/models';
